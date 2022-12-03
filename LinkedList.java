d65291629ac4614f10c42ef3c4fdc17cd9af27dc
Dinesh <87664339+Dineshp2903@users.noreply.github.com>
2022-12-03 22:21:47 +0530

    Update and rename List.java to LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,15 @@
+public class LinkedList
+{
+    Node head ;
+    class Node
+    {
+        public int data ;
+        public Node(int data)
+        {
+            this.data = data;
+            this.node = null;
+        }
+        
+    }
+        
+}
