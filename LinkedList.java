8c61653a0ca5856ff6c355bb281651058d31f9b6
Dinesh <87664339+Dineshp2903@users.noreply.github.com>
2022-12-03 23:08:48 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,7 +1,7 @@
 
 public class LinkedList
 {
-    Node head ;
+    Node head;
     static class Node
     {
         public int data ;
@@ -12,6 +12,64 @@ public class LinkedList
             this.next = null;
         }
         
+    }
+    
+    public static int size(LinkedList list)
+    {
+        int size = 0;
+        if(list.head == null)
+        {
+            return size;
+        }
+        else
+        {
+            Node node = list.head;
+            while(node.next != null)
+            {
+                size = size + 1;
+                node = node.next;
+            }
+        }
+        return size;
+        
+    }
+    
+    public static void insertfirst(LinkedList list,int data)
+    {
+        Node new_node = new Node(data);
+        if(list.head == null)
+        {
+            list.head = new_node;
+        }
+        else
+        {
+            new_node.next = list.head;
+            list.head = new_node;
+        }
+        
+    }
+    public static void insertAfter(LinkedList list,int posvalue,int data)
+    {
+        if(list.head == null)
+        {
+            System.out.println("List is Empty");
+        }
+        else
+        {
+            Node new_node = new Node(data);
+            Node node = list.head;
+            while(node.next != null)
+            {
+                if(node.data == posvalue)
+                {
+                    Node next = node.next;
+                    node.next = new_node;
+                    new_node.next = next;
+                    break;
+                }
+                node = node.next;
+            }
+        }
     }
     
     public static void insertlast(LinkedList list,int data)
@@ -35,7 +93,7 @@ public class LinkedList
     public static void printlist(LinkedList list)
     {
         Node node = list.head;
-        while(node.next != null)
+        while(node != null)
         {
             System.out.println(node.data);
             node = node.next;
@@ -52,7 +110,9 @@ public class LinkedList
         insertlast(list,3);
         insertlast(list,4);
         insertlast(list,5);
+        insertfirst(list,0);
         insertlast(list,6);
+        insertAfter(list,5,7);
         System.out.println("Printing List");
         printlist(list);
         
